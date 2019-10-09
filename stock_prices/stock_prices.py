3acbb380ce4ff6e6128892ac08c2145d3ebe4edf
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-10-09 13:39:04 -0400

    Update stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,23 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    # highest price - lowest price = max profit possible 
+    max_profit = prices[1] - prices[0]
+    lowest = prices[0]
+    # for every index for the length of the array
+    for i in range(1, len(prices)):
+        # if the current index is lower than the lowest number, make it the new lowest
+        if prices[i] < lowest:
+            lowest = prices[i]
+        # if the index price - the lowest is more than the current max profit, make it the new max profit  
+        elif prices[i] - lowest > max_profit:
+            max_profit = prices[i] - lowest
+    # return the max profit
+    return max_profit
+
+test = [10, 57, 25, 78, 111, 19]
+x = find_max_profit(test)
+print(f"\nYour max profit will be $", x)
 
 
 if __name__ == '__main__':
